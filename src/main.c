eb91df607c7a01b8af1dec1217e7246d656d5025
apollo78124 <98apollo@naver.com>
2022-11-20 02:34:13 -0800

    Packet header adding issue fixed

--- src/main.c
+++ src/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include "main_header.h"
 
-int main() {
+struct packet {
+    char flag[9];
+    int seq;
+    int ack;
 
-    char *dataToSend = "Hi";
-    char *packet = makePacketHeader("SYN", 12538, 1, 58789, dataToSend);
-    printf("%s\n", packet);
+};
+
+int main() {
+    const char *synFlag = "SYN";
+    const char *ackFlag = "ACK";
+    char dataToSend[1981];
+    char packetTemp[2041];
+
+    strcpy(dataToSend, "hi");
+    strcpy (packetTemp, makePacketHeader(synFlag, 12538, 1, 58789, dataToSend));
+    printf("%s\n", packetTemp);
+
+    strcpy(dataToSend, "Hihihihihihihihihihihihihihihihihi");
+    strcpy (packetTemp, makePacketHeader(synFlag, 12538, 1, 58789, dataToSend));
+    printf("%s\n", packetTemp);
+
+    strcpy(dataToSend, "Hi how are you?");
+    strcpy (packetTemp, makePacketHeader(ackFlag, 12540, 1, 58790, dataToSend));
+    printf("%s\n", packetTemp);
     return 0;
 }
 
@@ -13,19 +32,19 @@ int main() {
 /**
  * Packet header design:
  * In order
- * Flag 8 chars 0-7 , Sequence Number 16 chars 8-23, Ack Number 16 chars 24-39, Data Length 16 chars 40-55, Window 16 chars 56-71
- * Total 72 chars for packet header
- * Example header: 00000SYN0000000000012538000000000000000100000000000000020000000000058789
+ * Flag 8 chars 0-7 , Sequence Number 16 chars 8-23, Ack Number 16 chars 24-39, Data Length 5 chars 40-44, Window 16 chars 45-60
+ * Total 61 chars for packet header
+ * Example header: 00000SYN00000000000125380000000000000001000020000000000058789
  */
 char* makePacketHeader(char* flag, int seq, int ack, int window, char* dataToSend) {
 
-    char packetHeader[72 + 1] = "000000000000000000000000000000000000000000000000000000000000000000000000";
+    char packetHeader[61 + 1] = "0000000000000000000000000000000000000000000000000000000000000";
     int dataLength = strlen(dataToSend);
-    char resultingPacket[72 + dataLength + 1];
+    char resultingPacket[61 + dataLength + 1];
     //Convert sequence number, ack number, dataLength, window to string
     char seqInString[16];
     char ackInString[16];
-    char dataLengthInString[16];
+    char dataLengthInString[5];
     char windowInString[16];
 
     sprintf(seqInString, "%d", seq);
@@ -55,20 +74,22 @@ char* makePacketHeader(char* flag, int seq, int ack, int window, char* dataToSen
     }
 
     counter = 0;
-    for(int j = 56 - strlen(dataLengthInString); j < 56; j++) {
+    for(int j = 45 - strlen(dataLengthInString); j < 45; j++) {
         packetHeader[j] = dataLengthInString[counter];
         counter++;
     }
 
     counter = 0;
-    for(int j = 72 - strlen(windowInString); j < 72; j++) {
+    for(int j = 61 - strlen(windowInString); j < 61; j++) {
         packetHeader[j] = windowInString[counter];
         counter++;
     }
-
+    packetHeader[61] = '\0';
     strcpy(resultingPacket, packetHeader);
     strcat(resultingPacket, dataToSend);
-    char * result = resultingPacket;
+    char * result = {0};
+    result = resultingPacket;
+    result[61 + dataLength] = '\0';
     return result;
 }
 
